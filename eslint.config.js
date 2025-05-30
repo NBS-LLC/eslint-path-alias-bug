c36ae195129d734e1de5d024c58c5541ba2738e6
Nick Derevjanik <nick@networkbasedsolutions.com>
2025-05-29 23:39:13 -0700

    wip - using eslint import resolver ts

--- eslint.config.js
+++ eslint.config.js
@@ -1,50 +1,9 @@
-const coreConfig = require("eslint-config-expo/flat/utils/core.js");
-const expoConfig = require("eslint-config-expo/flat/utils/expo.js");
-const reactConfig = require("eslint-config-expo/flat/utils/react.js");
-const typescriptConfig = require("eslint-config-expo/flat/utils/typescript.js");
-const {
-  allExtensions,
-} = require("eslint-config-expo/flat/utils/extensions.js");
-
-const { defineConfig } = require("eslint/config");
-const globals = require("globals");
-
-module.exports = defineConfig([
-  ...coreConfig,
-  ...typescriptConfig,
-  // ...reactConfig,
-  // ...expoConfig,
-  // {
-  //   settings: {
-  //     "import/extensions": allExtensions,
-  //     "import/resolver": {
-  //       node: { extensions: allExtensions },
-  //     },
-  //   },
-  //   languageOptions: {
-  //     globals: {
-  //       ...globals.browser,
-  //       __DEV__: "readonly",
-  //       ErrorUtils: false,
-  //       FormData: false,
-  //       XMLHttpRequest: false,
-  //       alert: false,
-  //       cancelAnimationFrame: false,
-  //       cancelIdleCallback: false,
-  //       clearImmediate: false,
-  //       fetch: false,
-  //       navigator: false,
-  //       process: false,
-  //       requestAnimationFrame: false,
-  //       requestIdleCallback: false,
-  //       setImmediate: false,
-  //       window: false,
-  //       "shared-node-browser": true,
-  //     },
-  //   },
-  // },
-  // {
-  //   files: ["*.web.*"],
-  //   ignores: ["dist/*"],
-  // },
-]);
+module.exports = [
+  {
+    settings: {
+      "import/resolver": {
+        typescript: {},
+      },
+    },
+  },
+];
