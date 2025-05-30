4a54248ce5a773820bfdb6a9e1860bba8da124b4
Nick Derevjanik <nick@networkbasedsolutions.com>
2025-05-29 23:53:02 -0700

    wip - using eslint import resolver ts

--- eslint.config.js
+++ eslint.config.js
@@ -10,9 +10,7 @@ module.exports = [
     },
     settings: {
       "import/resolver": {
-        typescript: {
-          project: "./tsconfig.json",
-        },
+        typescript: true
       },
     },
     rules: {
