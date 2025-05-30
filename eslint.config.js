a091e529702663f5f0defb65ce89af334401612a
Nick Derevjanik <nick@networkbasedsolutions.com>
2025-05-29 23:46:37 -0700

    wip - using eslint import resolver ts

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,22 @@
+const importPlugin = require("eslint-plugin-import");
+const typescriptEslint = require("@typescript-eslint/eslint-plugin");
+
 module.exports = [
+  importPlugin.flatConfigs.recommended,
+  importPlugin.flatConfigs.errors,
   {
+    plugins: {
+      "@typescript-eslint": typescriptEslint,
+    },
     settings: {
       "import/resolver": {
-        typescript: {},
+        typescript: {
+          project: "./tsconfig.json",
+        },
       },
+    },
+    rules: {
+      "import/no-unresolved": "error",
     },
   },
 ];
